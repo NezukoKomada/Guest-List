cde730ff7ec7b2c51b7cf4262fd73c059aaefde9
NezukoKomada <159033240+NezukoKomada@users.noreply.github.com>
2024-02-05 17:56:30 +0400

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ function show()
 
 function sorting()
 	{
-		names_of_people.sort();           // add the sort function here
-		// .sort();
+		names_of_people.sort();
+		// add the sort function here // .sort();
 		var i= names_of_people.join("<br>");
 		console.log(names_of_people);		
 		document.getElementById("sorted").innerHTML=i.toString();
